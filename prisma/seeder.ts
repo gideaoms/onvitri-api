990c3ed8971e3c692462655a0198cd1b6220896b
Gideão <gideaoms@gmail.com>
2022-05-09 21:12:38 -0300

    add seed

---
+++ prisma/seeder.ts
@@ -0,0 +1,50 @@
+import prisma from '@/libs/prisma'
+import CryptoProvider from '@/providers/crypto'
+
+async function main() {
+  const cryptoProvider = CryptoProvider()
+  const city = await prisma.city.create({
+    data: {
+      id: 'b9bd02e7-7315-4df6-81b8-c3e150666c61',
+      name: 'Quinta do Sol',
+      initials: 'PR',
+    },
+  })
+  const user = await prisma.user.create({
+    data: {
+      id: '14028895-0d43-416a-8c8e-30b0cde42460',
+      name: 'João da Silva',
+      email: 'joao@mail.com',
+      roles: ['shopkeeper'],
+      status: 'active',
+      password: await cryptoProvider.hash('123456'),
+    },
+  })
+  await prisma.store.create({
+    data: {
+      id: '37bb3c9d-4cb7-40fd-95f2-37b768c605db',
+      owner_id: user.id,
+      city_id: city.id,
+      fantasy_name: 'Loja Vende Bem',
+      phone: {
+        country_area: '55',
+        area_code: '44',
+        number: '984999999',
+      },
+      neighborhood: 'Centro',
+      number: '546',
+      street: 'Rua Três Marias',
+      zip_code: '87265000',
+      status: 'active',
+    },
+  })
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,23 @@
 import { isNil } from '@/utils'
 
-function pick(key: string) {
-  if (isNil(process.env[key])) throw new Error(`Env ${key} is not defined`)
-  return process.env[key] as string
+const config = {
+  APP_ENV: process.env.APP_ENV,
+  APP_PORT: Number(process.env.APP_PORT),
+  APP_HOST: process.env.APP_HOST,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  DATABASE_USER: process.env.DATABASE_USER,
+  DATABASE_PASS: process.env.DATABASE_PASS,
+  DATABASE_PORT: process.env.DATABASE_PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+  TOKEN_SECRET: process.env.TOKEN_SECRET,
+  TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  AWS_S3_ENDPOINT: process.env.AWS_S3_ENDPOINT,
 }
 
-const config = {
-  APP_ENV: pick('APP_ENV'),
-  APP_PORT: Number(pick('APP_PORT')),
-  APP_HOST: pick('APP_HOST'),
-  DATABASE_NAME: pick('DATABASE_NAME'),
-  DATABASE_USER: pick('DATABASE_USER'),
-  DATABASE_PASS: pick('DATABASE_PASS'),
-  DATABASE_PORT: pick('DATABASE_PORT'),
-  DATABASE_URL: pick('DATABASE_URL'),
-  TOKEN_SECRET: pick('TOKEN_SECRET'),
-  TOKEN_EXPIRES_IN: pick('TOKEN_EXPIRES_IN'),
-  AWS_ACCESS_KEY_ID: pick('AWS_ACCESS_KEY_ID'),
-  AWS_SECRET_ACCESS_KEY: pick('AWS_SECRET_ACCESS_KEY'),
-  AWS_S3_ENDPOINT: pick('AWS_S3_ENDPOINT'),
-}
+Object.entries(config).forEach(([key, value]) => {
+  if (isNil(value)) throw new Error(`Env ${key} is not defined`)
+})
 
 export default config
