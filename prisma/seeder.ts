a552e044dd8d93466a382c45b248482eb7033864
Gideão <gideaoms@gmail.com>
2022-05-11 11:00:35 -0300

    include src in tsconfig

--- prisma/seeder.ts
+++ prisma/seeder.ts
@@ -1,5 +1,5 @@
-import prisma from '@/libs/prisma'
-import CryptoProvider from '@/providers/crypto'
+import prisma from '../src/libs/prisma'
+import CryptoProvider from '../src/providers/crypto'
 
 async function main() {
   const cryptoProvider = CryptoProvider()
