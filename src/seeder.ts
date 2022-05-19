16a137299b07e2d6df965f58550360e68cec2833
Gideão <gideaoms@gmail.com>
2022-05-19 09:07:34 -0300

    update seeder

--- src/seeder.ts
+++ src/seeder.ts
@@ -1,50 +1,50 @@
-import prisma from '../src/libs/prisma'
-import CryptoProvider from '../src/providers/crypto'
+import prisma from '@/libs/prisma';
+import CryptoProvider from '@/providers/crypto';
 
 async function main() {
-  const cryptoProvider = CryptoProvider()
+  const cryptoProvider = CryptoProvider();
   const city = await prisma.city.create({
     data: {
       id: 'b9bd02e7-7315-4df6-81b8-c3e150666c61',
-      name: 'Quinta do Sol',
-      initials: 'PR',
+      name: 'City 1',
+      initials: 'AA',
     },
-  })
+  });
   const user = await prisma.user.create({
     data: {
       id: '14028895-0d43-416a-8c8e-30b0cde42460',
-      name: 'João da Silva',
-      email: 'joao@mail.com',
+      name: 'Shopkeeper 1',
+      email: 'store1@mail.com',
       roles: ['shopkeeper'],
       status: 'active',
       password: await cryptoProvider.hash('123456'),
     },
-  })
+  });
   await prisma.store.create({
     data: {
       id: '37bb3c9d-4cb7-40fd-95f2-37b768c605db',
       owner_id: user.id,
       city_id: city.id,
-      fantasy_name: 'Loja Vende Bem',
+      fantasy_name: 'Store 1',
       phone: {
         country_code: '55',
         area_code: '44',
-        number: '984999999',
+        number: '333333333',
       },
-      neighborhood: 'Centro',
+      neighborhood: 'Center',
       number: '546',
-      street: 'Rua Três Marias',
+      street: 'Any Street',
       zip_code: '87265000',
       status: 'active',
     },
-  })
+  });
 }
 
 main()
   .catch((e) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
